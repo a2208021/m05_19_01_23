0a655ae58ee246302b403136c89b674d688a38d8
Edu <pewpet@gmail.com>
2023-01-19 15:47:46 +0100

    I think this will make your code GREAT (AGAIN)

--- m05_19_01_23/Program.cs
+++ m05_19_01_23/Program.cs
@@ -4,5 +4,11 @@ internal class Program
     {
         Console.WriteLine("Hello Montilivi!");
         Console.WriteLine("Estoy aprendiendo a utilizar GitHub");
+        Console.WriteLine("look at my number: "+Randomized());
+    }
+    public static int Randomized() {
+        Random dog = new Random();
+
+        return dog.Next(1, 7);
     }
 }
